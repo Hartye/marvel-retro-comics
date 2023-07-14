e8d499c46c7c686b3492d785a72b87126b4d40b5
Hartye <hartye.lilith@gmail.com>
2023-07-14 11:37:03 +0100

    first changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Public API key = 86cb2be8aecbf307ae51cbfb3804be73
+// Private API key = cc14c3d6270f17d6060b94a355accd3988e23ae8
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      footerCopy: '',
+      apiKey: '86cb2be8aecbf307ae51cbfb3804be73'
+    }
+  }
+
+  async componentDidMount() {
+    let url =
+      'http://gateway.marvel.com/v1/public/' +
+      'comics' + // characters | comics | creators | events | series | stories
+      '?apikey=86cb2be8aecbf307ae51cbfb3804be73';
+    let req = new Request(url);
+    await fetch(req)
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        this.setState({
+          footerCopy: res.attributionText
+        })
+
+        for (let i = 0; i < res.data.count; i++) {
+          let imageURL = res.data.results[i].thumbnail.path + '/portrait_medium.' + res.data.results[i].thumbnail.extension;
+          let htmlContent =
+            `
+            <div className="App-comic-instance" id="${i}">
+              <img className="App-instance-image" src="${imageURL}" alt="Comic" />
+              <h1>${res.data.results[i].title}</h1>
+            </div>
+            `;
+          console.log(imageURL);
+          document.querySelector(".App-content-instances").innerHTML += htmlContent;
+        }
+      })
+  }
+
+  render() {
+    return (
+      <div className="App-content-instances">
+
+      </div>
+    );
+  }
 }
 
 export default App;
